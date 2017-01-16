94423c9b6ebd4c5467d0416be77b654e59c77278
Christopher Stone <eeyces@ad.nottingham.ac.uk>
2017-01-16 10:49:50 +0000

    added code to record black and white values

--- line_sensor_demo2/line_sensor_demo2.ino
+++ line_sensor_demo2/line_sensor_demo2.ino
@@ -3,11 +3,14 @@
 uchar t;
 //void send_data(short a1,short b1,short c1,short d1,short e1,short f1);
 uchar data[16];
+uchar blackdata[8];
+uchar whitedata[8];
 void setup()
 {
  Wire.begin(); // join i2c bus (address optional for master)
  Serial.begin(9600); // start serial for output
  t = 0;
+ Serial.println("Recording black at 25, white at 50");
 }
 void loop()
 {
@@ -21,12 +24,34 @@ void loop()
  t = 0;
  }
 
+//printData();
+if(millis() < 5000){
+  Serial.println(millis()/100);
+}
 
+ if(millis()>2500 && millis()<2600){
+    for(int i=0; i<=7; i++){
+      blackdata[i] = data[2*i];
+   }
+   Serial.print("Recorded black values as ");
+   printData();
+ }
+ if(millis()>5000 && millis()<5100){
+    for(int i=0; i<=7; i++){
+      whitedata[i] = data[2*i];
+   }
+   Serial.print("Recorded white values as ");
+   printData();
+ }
+ 
+ delay(500);
+}
+
+void printData(){
  for(int i=0; i<=14; i+=2){
   Serial.print(data[i]);
   Serial.print(" ");
  }
  Serial.println("");
- 
- delay(500);
 }
+
